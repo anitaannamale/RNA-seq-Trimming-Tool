457031558fcb32573735d63598a7455044f1b034
anitaannamale <anita.annamale@gmail.com>
2015-06-05 14:40:53 +0200

    Update argparse_commandline.py

--- src/argparse_commandline.py
+++ src/argparse_commandline.py
@@ -42,13 +42,19 @@ def Trimmomatic_parser():
 	
 	parser = argparse.ArgumentParser( 
 		formatter_class=RawTextHelpFormatter,
-		description='A python program that launch Trimmomatic Tool : A Adapter and Quality Trimming Tools for high throughput sequencing data',
+		description='A python program that launch Trimmomatic Tool : A Adapter \
+and Quality Trimming Tools for high throughput sequencing data',
 		epilog="\nSome examples:\n\n\t\
 'python Filtrage.py -h' \t for help\n\t\
-'python Filtrage.py --XML' \t to launch trimmomatic using parameters from the XML file\n\t\
-'python Filtrage.py PE read_1.fastq read_2.fastq -illuminaclip fasta-file.fa:2:10:30' \t for adapter trimming\n\t\
-'python Filtrage.py SE read_1.fq -slidingwindow 10:30 -leading 30 -minlen 36' \t for quality trimming\n\t\
-'python Filtrage.py PE read_1.fq.bz2 read_2.fq.bz2 -illuminaclip fasta-file.fa:2:10:30 -slidingwindow 10:30 -minlen 36' \t for adapter and quality trimming.")
+'python Filtrage.py --XML' \t to launch trimmomatic using parameters from the \
+XML file\n\t\
+'python Filtrage.py PE read_1.fastq read_2.fastq -illuminaclip \
+fasta-file.fa:2:10:30' \t for adapter trimming\n\t\
+'python Filtrage.py SE read_1.fq -slidingwindow 10:30 -leading 30 -minlen 36' \
+\t for quality trimming\n\t\
+'python Filtrage.py PE read_1.fq.bz2 read_2.fq.bz2 -illuminaclip \
+fasta-file.fa:2:10:30 -slidingwindow 10:30 -minlen 36' \t for adapter and \
+quality trimming.")
 
 	parser.add_argument("--XML", 
 						action='store_const', 
@@ -59,13 +65,16 @@ def Trimmomatic_parser():
 						type=str, 
 						nargs='?',
 						action='store', 
-						help = "layout of reads.\n  Usage: 'SE' (single-ends) or 'PE' (paired-ends)")
+						help = "layout of reads.\n  Usage: 'SE' (single-ends) \
+or 'PE' (paired-ends)")
 	
 	parser.add_argument("input", 
 						type=str, 
 						nargs= '*',
 						action='store',
-						help = "input read file.\n  Usage:\n  For SE 'readfile.fastq' || 'readfile.fq.gz'\n  For PE 'read_1.fastq read_2.fastq' || 'read_1.fastq.gz/bz2 read_2.fastq.gz/bz2'")
+						help = "input read file.\n  Usage:\n  For SE \
+'readfile.fastq' || 'readfile.fq.gz'\n  For PE 'read_1.fastq read_2.fastq' || \
+'read_1.fastq.gz/bz2 read_2.fastq.gz/bz2'")
 	
 	parser.add_argument("-threads",
 						type=int,
@@ -81,52 +90,63 @@ def Trimmomatic_parser():
 	parser.add_argument("-illuminaclip",
 						type=str,
 						action='store',
-						help= 'Adapter trimming.\n  Usage: -illuminaclip <fastaWithAdaptersEtc>:<seed mismatches>:<palindrome clip threshold>:<simple clip threshold>\n  Recommended: fasta-file:2:30:10')
+						help= 'Adapter trimming.\n  Usage: -illuminaclip \
+<fastaWithAdaptersEtc>:<seed mismatches>:<palindrome clip threshold>:<simple \
+clip threshold>\n  Recommended: fasta-file:2:30:10')
 	
 	parser.add_argument("-slidingwindow",
 						type=str,
 						action='store',
-						help= 'Quality trimming.\n  Usage: -slidingwindow <window-size>:<required-quality>\n  Recommended: 4:30 or 10:30')
+						help= 'Quality trimming.\n  Usage: -slidingwindow \
+<window-size>:<required-quality>\n  Recommended: 4:30 or 10:30')
 	
 	parser.add_argument("-maxinfo",
 						type=str,
 						action='store',
-						help='Adaptative quality trimming.\n  Usage: -maxinfo <target-length>:<strictness>')
+						help='Adaptative quality trimming.\n  Usage: -maxinfo \
+<target-length>:<strictness>')
 	
 	parser.add_argument("-leading",
 						type=int,
 						action='store',
-						help="minimum quality required to keep a base in 5' end.\n  Usage: '-leading <required-quality>'")
+						help="minimum quality required to keep a base in 5' \
+end.\n  Usage: '-leading <required-quality>'")
 	
 	parser.add_argument("-trailing",
 						type=int,
 						action='store',
-						help="minimum quality required to keep a base in 3' end.\n  Usage: '-trailing <required-quality>'")
+						help="minimum quality required to keep a base in 3' \
+end.\n  Usage: '-trailing <required-quality>'")
 	
 	parser.add_argument("-crop",
 						type=int,
 						action='store',
-						help="number of base to remove from the end of the read.\n  Usage: '-crop <number>'")
+						help="number of base to remove from the end of the \
+read.\n  Usage: '-crop <number>'")
 	
 	parser.add_argument("-headcrop",
 						type=int,
 						action='store',
-						help="number of base to remove from the start of the read.\n  Usage: '-head <number>'")
+						help="number of base to remove from the start of the \
+read.\n  Usage: '-head <number>'")
 	
 	parser.add_argument("-minlen",
 						type=int,
 						action='store',
-						help="minimum required length of a read to be kept.\n  Usage: '-minlen <length>'")
+						help="minimum required length of a read to be kept.\n \
+ Usage: '-minlen <length>'")
 		
 	parser.add_argument("-tophred33",
 						action='store_const',
 						const='TOPHRED33',
-						help="(re)encodes the quality part of the FASTQ file to base 33.\n  Usage: '-tophred33'")
+						help="(re)encodes the quality part of the FASTQ file to\
+ base 33.\n  Usage: '-tophred33'")
 	
 	parser.add_argument("-tophred64",
 						action='store_const',
 						const='TOPHRED64',
-						help="(re)encodes the quality part of the FASTQ file to base 64.\n  Usage: '-tophred64'")
+						help="(re)encodes the quality part of the FASTQ file to\
+ base 64.\n  Usage: '-tophred64'")
 	
 	return parser
 
@@ -134,8 +154,8 @@ def Trimmomatic_parser():
 
 def check_layout(arg):
 	"""
-	Function that check that a layout have been entered and it's either 'SE'(Single-Ends) 
-	or 'PE'(Paired-Ends).
+	Function that check that a layout have been entered and it's either 'SE'
+	(Single-Ends) or 'PE'(Paired-Ends).
 	
 	Takes one argument :
 		arg [dict] : dictionnary containning all the command argument entries.
@@ -156,7 +176,8 @@ def check_layout(arg):
 		if(layout == 'PE' or layout == 'SE'):
 			return layout
 		else :
-			sys.exit("Error : Layout can only be 'SE' (for single-end) or 'PE' (for paired-ends).")
+			sys.exit("Error : Layout can only be 'SE' (for single-end) or 'PE'\
+ (for paired-ends).")
 
 
 
@@ -196,7 +217,8 @@ def check_input(arg):
 	
 	Returns:
 		- 1 [integer] : if input files extension is '.fastq' or '.fq'
-		- 2 [integer] : if input files extension is '.fastq/fq.bz2' or '.fastq/fq.gz'
+		- 2 [integer] : if input files extension is '.fastq/fq.bz2' 
+						or '.fastq/fq.gz'
 		or
 		quit
 	"""
@@ -227,7 +249,8 @@ def check_phred(arg):
 	if phred != None :
 		# if it's 33 or 64 return 1 or quit
 		if not (phred == 33 or phred == 64):
-			sys.exit("Error : Value for the option 'phred' can only be '33' or '64'.")
+			sys.exit("Error : Value for the option 'phred' can only be '33' or \
+'64'.")
 		else :
 			return 1
 
@@ -256,7 +279,8 @@ def check_illuminaclip(arg):
 		
 		# verify the number of arguments between ':'.
 		if not len(illum) == 4:
-			sys.exit("Error : Option illuminaclip must avec 4 elements between ':'")
+			sys.exit("Error : Option illuminaclip must avec 4 elements between \
+':'")
 		
 		# get fasta file
 		fasta_file=illum[0]
@@ -274,11 +298,13 @@ def check_illuminaclip(arg):
 
 		# check that an integer is entered for palindrome clip threshold
 		if not illum[2].isdigit():
-			sys.exit("Error: Value for palindrome clip threshold must be an integer")
+			sys.exit("Error: Value for palindrome clip threshold must be an \
+integer")
 
 		# check that an integer is entered for single clip threshold
 		if not illum[3].isdigit():
-			sys.exit("Error: Value for simple clip threshold must be an integer")
+			sys.exit("Error: Value for simple clip threshold must be an \
+integer")
 		
 		return 1
 		
@@ -310,15 +336,18 @@ def check_slidingwindow(arg):
 		
 		# verify the number of arguments between ':'.
 		if not len(slidw) == 2 :
-			sys.exit("Error : Option silidingwindow must have 2 elements between ':'")
+			sys.exit("Error : Option silidingwindow must have 2 elements between\
+ ':'")
 
 		# check that an integer is entered for window size
 		if not slidw[0].isdigit():
-			sys.exit("Error : Value for window-size (sliding-window) must be an integer")
+			sys.exit("Error : Value for window-size (sliding-window) must be an\
+ integer")
 	
 		# check that an integer is entered for required quality
 		if not slidw[1].isdigit():
-			sys.exit("Error : Value for required-quality (sliding-window) must be an integer")
+			sys.exit("Error : Value for required-quality (sliding-window) must\
+ be an integer")
 		
 		return 1
 	
@@ -349,18 +378,21 @@ def check_maxinfo(arg):
 		
 		# verify the number of arguments between ':'.
 		if not len(maxinfo) == 2:
-			sys.exit("Error : Option maxinfo must have two elements between ':'")
+			sys.exit("Error : Option maxinfo must have two elements between \
+':'")
 
 		# check that an integer is entered for targer length
 		if not maxinfo[0].isdigit():
-			sys.exit("Error : Value for target-length (maxinfo) must be an integer")
+			sys.exit("Error : Value for target-length (maxinfo) must be an \
+integer")
 
 
 		# convert strictness value into float
 		nb = float(maxinfo[1])
 		# check if it's between 0 and 1
 		if (nb < 0 or nb > 1):
-			sys.exit("Error : Value for strictness must be a float between 0 and 1")
+			sys.exit("Error : Value for strictness must be a float between 0 \
+and 1")
 		
 		return 1
 	
@@ -428,7 +460,8 @@ def argparse_commandline_step_1(arg,nb, inout):
 	cmd = 'java -jar trimmomatic-0.33.jar'
 
 	# adding layout and input and output files
-	cmd, inout = commandline_input_output(arg, cmd, nb, inout) # from commandline module
+	cmd, inout = commandline_input_output(arg, cmd, nb, inout) # from commandline
+															   # module
 
 	# if illuminaclip step, add argument to command line or else skip the step
 	if(arg['illuminaclip'] != None):
@@ -443,7 +476,8 @@ def argparse_commandline_step_1(arg,nb, inout):
 
 def argparse_commandline_step_2(arg, nb, inout):
 	"""
-	Function that generate step1 2 (quality trimming) command line for Trimmomatic.
+	Function that generate step1 2 (quality trimming) command line for 
+	Trimmomatic.
 	
 	Takes 3 argument :
 		arg [dict] : dictionnary containning all the command argument entries.
